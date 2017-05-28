2ca3350d8078f45a4f7b06eb6b29792f6d655127
Maks Grabowy <maksgrabowy@gmail.com>
2017-05-28 20:04:12 +0200

    Add files via upload

---
+++ modules/ARDUINO/temper_ext.ino
@@ -0,0 +1,32 @@
+float temperature;
+#include <Wire.h>  
+#include <OneWire.h>
+#include <DS18B20.h>
+#define ONEWIRE_PIN 2   //Pin is changable
+byte address[8] = {0x28, 0xFF, 0x4C, 0xCF, 0x33, 0x16, 0x4, 0x3E};  //Address is unique for every device, and it has to be read before putting into the code
+
+OneWire onewire(ONEWIRE_PIN);
+DS18B20 sensors(&onewire);
+
+void setup(){
+   while(!Serial);
+sensors.begin();
+  sensors.request(address);
+   Serial.begin(9600);  
+
+}
+ 
+void loop(){
+  //External termometer
+  
+   if (sensors.available())
+  {
+    temperature = sensors.readTemperature(address);
+
+    Serial.print(temperature);
+    Serial.println(F(" 'C - external"));
+
+    sensors.request(address);
+  }
+   delay(1000);
+}                         
---
+++ modules/ARDUINO/temper_int_analog.ino
@@ -0,0 +1,22 @@
+int sens = A0;   //Analog pin is changeable
+float VOLT;
+float TEMP;
+#include <Wire.h> 
+
+void setup(){
+   while(!Serial);
+   Serial.begin(9600);  
+   
+
+}
+ 
+void loop(){
+ 
+  //Internal termometer
+  int temp = analogRead(sens);      
+  VOLT = (temp * 5.0) / 1024.0;        
+  TEMP = VOLT  * 100;    
+   Serial.print(TEMP);
+   Serial.println(F(" 'C - internal"));
+   delay(1000);
+}                         
