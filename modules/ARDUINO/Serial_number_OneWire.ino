34e97a54f6160271a6e1d5cab76c9029defa3f5e
Maks Grabowy <maksgrabowy@gmail.com>
2017-05-30 21:40:31 +0200

    Add files via upload

---
+++ modules/ARDUINO/Serial_number_OneWire.ino
@@ -0,0 +1,37 @@
+//Serial number will be displayed in Serial
+#include <OneWire.h>
+const byte ONEWIRE_PIN = 2; //Pin number
+OneWire onewire(ONEWIRE_PIN);
+void setup()
+{
+  while(!Serial);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  byte address[8];
+  onewire.reset_search();
+  while(onewire.search(address))
+  {
+    if (address[0] != 0x28)
+      continue;
+
+    if (OneWire::crc8(address, 7) != address[7])
+    {
+      Serial.println(F("wrong adress, check the conections"));
+      break;
+    }
+
+    for (byte i=0; i<8; i++)
+    {
+      Serial.print(F("0x"));
+      Serial.print(address[i], HEX);
+
+      if (i < 7)
+        Serial.print(F(", "));
+    }
+    Serial.println();
+  }
+  while(1);
+}
